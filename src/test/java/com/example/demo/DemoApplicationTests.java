ac57b25956b4aa4b972c48ed753b1d318008a29a
Jose Cruz Florian Najera <cflorian@tak.gt>
2024-05-28 16:22:58 -0600

    test of testing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.IOException;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import com.example.cashcard.CashCard;
+
+@JsonTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void myFirstTest() {
+	   assertThat(42).isEqualTo(42);
 	}
 
+	@Autowired
+    private JacksonTester<CashCard> json;
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = new CashCard(99L, 123.45);
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(99);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
+             .isEqualTo(123.45);
+    }
 }
